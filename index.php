28741ab151ac663ebf80b36c8c058cb8e6bbf634
sankalpalokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2024-08-27 23:06:17 +0900

    add logo

--- index.php
+++ index.php
@@ -15,12 +15,14 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg bg-white fixed-top">
   <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+        <img src="img/Logo.png" alt="logo" style="width: 100px;">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto">
+      <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
@@ -37,12 +39,11 @@
           <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link btn text-white bg-dark px-3" href="#">Reviews</a>
+          <a class="nav-link btn text-white bg-dark px-3 rounded-5" href="#">Reviews</a>
         </li>
       
       </ul>
-      <button class="btn btn-outline-success" type="submit">Search</button>
-     
+      
     </div>
   </div>
 </nav>
