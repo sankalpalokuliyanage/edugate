38e64cc545f6f1f28e653add341870fc07024cd3
sankalpalokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2024-08-28 22:51:31 +0900

    make database connection

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+    $uname = "root";
+    $pass = "";
+    $host = "localhost";
+    $db = "eg_xrst";
+
+    $conn = mysqli_connect("$host", "$uname", "$pass", "$db") or die("DB connection error");
+
+?>
