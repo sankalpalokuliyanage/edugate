141ec6c004f78b2efd9d03123135597bd93ab20d
sankalpalokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2024-10-03 23:31:21 +0900

    update

---
+++ panel.php
@@ -0,0 +1,13 @@
+<?php 
+    include('panelincludes/header.php');
+    include('panelincludes/nav.php');
+
+?>
+
+
+
+<?php 
+    include('panelincludes/scripts.php');
+    include('panelincludes/footer.php');
+
+?>
---
+++ panelincludes/scripts.php
@@ -0,0 +1,17 @@
+   <!-- Bootstrap core JavaScript-->
+   <script src="ADMIN/vendor/jquery/jquery.min.js"></script>
+    <script src="ADMIN/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="ADMIN/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="ADMIN/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="ADMIN/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="ADMIN/js/demo/chart-area-demo.js"></script>
+    <script src="ADMIN/js/demo/chart-pie-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
